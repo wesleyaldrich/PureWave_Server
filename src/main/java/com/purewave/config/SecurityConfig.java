c125d7d7b6cf072adc3ea27dd17ef8b482a85b29
Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
2025-01-06 13:40:49 +0700

    Caching implemented

--- src/main/java/com/purewave/config/SecurityConfig.java
+++ src/main/java/com/purewave/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-﻿package com.purewave.config;
+package com.purewave.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/purewave/service/PostService.java
+++ src/main/java/com/purewave/service/PostService.java
@@ -3,6 +3,9 @@ package com.purewave.service;
 import com.purewave.model.Post;
 import com.purewave.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,19 +16,23 @@ public class PostService {
     private PostRepository postRepository;
 
     // Fetch only primary posts (attachedTo == null)
+    @Cacheable(value = "posts")
     public List<Post> getPrimaryPosts() {
         return postRepository.findByAttachedToIsNull();
     }
 
+    @Cacheable(value = "posts", key = "#postId")
     // Fetch replies for a given post ID
     public List<Post> getRepliesByPostId(String postId) {
         return postRepository.findByAttachedTo(postId);
     }
 
+    @CachePut(value = "posts", key = "#result.id")
     public Post savePost(Post post) {
         return postRepository.save(post);
     }
 
+    @CacheEvict(value = "posts", key = "#id")
     public void deletePost(String id) {
         postRepository.deleteById(id);
     }
