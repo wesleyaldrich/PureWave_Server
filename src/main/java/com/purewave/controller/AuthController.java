ff70a71b9e3c9a88bf2dfa13988a083a1ca6157a
Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
2025-02-01 22:21:59 +0700

    CHECKPOINT: Add user profile image on top right

--- src/main/java/com/purewave/controller/AuthController.java
+++ src/main/java/com/purewave/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.purewave.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +21,13 @@ public class AuthController {
             return ResponseEntity.ok(Collections.singletonMap("token", authToken));
         }
         return ResponseEntity.ok(Collections.singletonMap("token", null));
+    }
+
+    @GetMapping("/email")
+    public Map<String, String> getUserInfo(@AuthenticationPrincipal OidcUser principal) {
+        if (principal == null) {
+            return Map.of("error", "null");
+        }
+        return Map.of("email", principal.getEmail());
     }
 }
